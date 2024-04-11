0fa1186ef80b2bbec200c0a2ba5ad8209c2a0982
Oliverkkxd <99224557+Oliverkkxd@users.noreply.github.com>
2024-04-11 17:32:25 -0300

    Add files via upload

---
+++ numero em letras else if.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main()
+{
+    //Variaveis 
+    int a;
+
+printf("Digite o primeiro número:");
+scanf("%i", &a);
+
+
+//bloco do if 
+if(a >= 86){
+    printf("\nA");
+}
+else if(a >= 61){
+    printf("\n B");
+}
+ else if(a >= 36){
+    printf("\n C");
+}  
+ else if(a >= 1){
+    printf("\n D");
+}   
+  else{
+    printf("\n E");
+}
+    
+}
+
+
