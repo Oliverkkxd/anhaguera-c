932ce49825d698a117d3016edcb9f7c2e9faa810
Oliverkkxd <99224557+Oliverkkxd@users.noreply.github.com>
2024-04-11 17:33:21 -0300

    Add files via upload

---
+++ Meses while.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+int main()
+{
+    
+    int mes = 1;
+    while(mes >= 1 && mes <= 12){
+        
+    printf("%i-janeiro \n", mes);
+    mes++;
+    printf("%i-fevereiro\n", mes);
+    mes++;
+    printf("%i-março\n", mes);
+    mes++;
+    printf("%i abril\n", mes);
+    mes++;
+    printf("%i-maio\n", mes);
+    mes++;
+    printf("%i-junho\n", mes);
+    mes++;
+    printf("%i-julho\n", mes);
+    mes++;
+    printf("%i-agosto\n", mes);
+    mes++;
+    printf("%i-setembro\n", mes);
+    mes++;
+    printf("%i-outubro\n", mes);
+    mes++;
+    printf("%i-novembro\n", mes);
+    mes++;
+    printf("%i-dezembro\n", mes); 
+    mes++;
+    }
+   
+
+    return 0;
+}
+
